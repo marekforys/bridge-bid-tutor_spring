c892e1f83c566e630673be2bb7cd2451c7f2b2a6
Marek Forys <mforys73@gmail.com>
2025-06-30 23:31:32 +0200

    Skeleton of sample Spring Boot app
    Signed-off-by: Marek Forys <mforys73@gmail.com>

---
+++ src/test/java/com/voris/bridge/biddingtutor/demo/BridgeBriddingTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voris.bridge.biddingtutor.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeBriddingTutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
