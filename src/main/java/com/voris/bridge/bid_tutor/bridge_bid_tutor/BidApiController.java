82fa21d5eb0524715b12214bf9292c2dd9fc7775
Marek Forys <mforys73@gmail.com>
2025-07-01 11:44:35 +0200

    BidApiController added
    Signed-off-by: Marek Forys <mforys73@gmail.com>

---
+++ src/main/java/com/voris/bridge/bid_tutor/bridge_bid_tutor/BidApiController.java
@@ -0,0 +1,28 @@
+package com.voris.bridge.bid_tutor.bridge_bid_tutor;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BidApiController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
--- src/main/java/com/voris/bridge/bid_tutor/bridge_bid_tutor/BridgeBidTutorApplication.java
+++ src/main/java/com/voris/bridge/bid_tutor/bridge_bid_tutor/BridgeBidTutorApplication.java
@@ -13,23 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
-@RestController
 public class BridgeBidTutorApplication {
-
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
 
 	public static void main(String[] args) {
 		SpringApplication.run(BridgeBidTutorApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
 
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
 }
