de524ec015e71446ca6aa49a341341f56a00a52d
Marek Forys <mforys73@gmail.com>
2025-07-01 11:45:45 +0200

    Folder for tests added with just one now
    Signed-off-by: Marek Forys <mforys73@gmail.com>

---
+++ src/test/java/com/voris/bridge/bid_tutor/bridge_bid_tutor/BridgeBidTutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voris.bridge.bid_tutor.bridge_bid_tutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BridgeBidTutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
